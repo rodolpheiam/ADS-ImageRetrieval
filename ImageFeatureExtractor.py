825be301375535f436a16c442461334d7a9e4e2b
Peng Ding <dingpeng09@gmail.com>
2012-04-15 21:13:09 +0800

    update output file format

--- ImageFeatureExtractor.py
+++ ImageFeatureExtractor.py
@@ -2,10 +2,15 @@ from PIL import Image
 import glob, math
 
 def main():
-    output_file = open('output', 'w')
+    feature_file = open('color_feature.txt', 'w')
+    feature_file.write('tech nmos' + '\n')
+    feature_file.write('<< polysilicon >>' + '\n')
+    image_list_file = open('imagelist.txt', 'w')
     for filename in glob.glob('image/*.JPEG'):
-        hg = get_feature(4, filename)
-        output_file.write(filename + ' ' + str.join(' ', [str(i) for i in hg]) + '\n')
+        hg = get_feature(12, filename)
+        feature_file.write('rect' + ' ' + str.join(' ', [str(i) for i in hg]) + '\n')
+        image_list_file.write(filename + '\n')
+    feature_file.write('<< end >>')
 
 def get_feature(dimension, filename):
     im = Image.open(filename)
@@ -22,7 +27,10 @@ def divide_histogram(dimension, histogram):
     return hg
 
 def equalize_histogram(histogram):
-    hg = histogram
+    rate = float(sum(histogram)) / 500000
+    hg = []
+    for i in histogram:
+        hg.append(int(i / rate))
     return hg
 
 if __name__ == '__main__':
