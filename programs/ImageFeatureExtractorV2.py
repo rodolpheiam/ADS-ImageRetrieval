27c01d6668e28a4301d70cdb0f88a0e2822ee7cd
Peng Ding <dingpeng09@gmail.com>
2012-04-19 13:34:10 +0800

    add new ImageFeatureExtractor

--- programs/ImageFeatureExtractorV2.py
+++ programs/ImageFeatureExtractorV2.py
@@ -1,36 +1,39 @@
 from PIL import Image
 import glob, math
 
-def generate_feature(dimension):
+def generate_feature(r_dimension, g_dimension, b_dimension):
+    dimension = r_dimension * g_dimension * b_dimension
     feature_file = open('color_feature_%d.txt' % dimension, 'w')
     feature_file.write('tech histogram' + '\n')
     feature_file.write('<< polysilicon >>' + '\n')
-    #image_list_file = open('imagelist.txt', 'w')
+    image_list_file = open('imagelist_%d.txt' % dimension, 'w')
     for filename in glob.glob('image/*.JPEG'):
-        hg = get_feature(dimension, filename)
+        hg = get_histogram(r_dimension, g_dimension, b_dimension, filename)
         feature_file.write('rect ' + str.join(' ', [str(i) for i in hg]) + '\n')
-        #image_list_file.write(filename[6:] + '\n')
+        image_list_file.write(filename[6:] + '\n')
     feature_file.write('<< end >>')
 
-def get_feature(dimension, filename):
-    im = Image.open(filename)
-    hg = im.histogram()
-    return merge_histogram(dimension, hg)
-
-def merge_histogram(dimension, histogram):
-    full_length = len(histogram)
-    part_length = int(math.ceil(full_length / float(dimension)))
-    hg = [sum(histogram[i : i+part_length]) for i in xrange(0, full_length, part_length)]
+def get_histogram(r_dimension, g_dimension, b_dimension, filename):
+    image = Image.open(filename)
+    data = list(image.getdata())
+    full_len = 0x100
+    r_piece_len = int(math.ceil(full_len / float(r_dimension)))
+    g_piece_len = int(math.ceil(full_len / float(g_dimension)))
+    b_piece_len = int(math.ceil(full_len / float(b_dimension)))
+    hg = [0 for i in xrange(r_dimension * g_dimension * b_dimension)]
+    for pixel in data:
+        hg[pixel[0]/r_piece_len * g_dimension * b_dimension + pixel[1]/g_piece_len * b_dimension + pixel[2]/b_piece_len] += 1
     return equalize_histogram(hg)
 
 def equalize_histogram(histogram):
     rate = sum(histogram) / 10000.0
-    hg = [int(i / rate) for i in histogram]
-    return hg
+    histogram = [int(i / rate) for i in histogram]
+    return histogram
 
 if __name__ == '__main__':
-    generate_feature(4)
-    generate_feature(8)
-    generate_feature(12)
-    generate_feature(16)
-    generate_feature(20)
+    generate_feature(1, 2, 2)
+    generate_feature(2, 2, 2)
+    generate_feature(2, 2, 3)
+    generate_feature(2, 2, 4)
+    generate_feature(2, 2, 5)
+
